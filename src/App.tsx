f64679fb63b260e399366944172097c9d9be8b3a
Yelkira <email@email.com>
2023-01-03 15:05:10 +0300

    map + home work

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ function App() {
     const topCars = [
         {manufacturer:'BMW', model:'m5cs'},
         {manufacturer:'Mercedes', model:'e63s'},
+        {manufacturer:'Audi', model:'rs6'},
+        {manufacturer:'BMW', model:'m5cs'},
+        {manufacturer:'Mercedes', model:'e63s'},
+        {manufacturer:'Audi', model:'rs6'},
+        {manufacturer:'BMW', model:'m5cs'},
+        {manufacturer:'Mercedes', model:'e63s'},
         {manufacturer:'Audi', model:'rs6'}
     ]
     return (
